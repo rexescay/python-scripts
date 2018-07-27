5b2a4b94afbf5222403fa66205ded16a1d6cc078
rexescay <rx.professional@protonmail.com>
2018-07-27 19:43:04 -0400

    Psudocode outline of media_sort.py

--- media_sort.py
+++ media_sort.py
@@ -2,3 +2,19 @@
 
 # image_sort.py - Walks directories from root folder and sorts files
 # into apropriate directories
+# python3 image_sort.py <path/to/root/directory>
+
+import os, random, re, sys
+
+if len(sys.argv) == 2:
+	root_dir = sys.argv[1]
+else:
+	print('# python3 image_sort.py <path/to/root/directory>')
+	sys.exit()
+
+# Walk filesystem from chosen root directory
+# Check each file suffix using regex
+# each new suffix is saved in a list
+# new directory is created for each item in list
+# option to randomize filenames, not changing suffix
+# each file is moved into appropriate directory
