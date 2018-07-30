c402b751c359edeb3951f51ce0ffbe9d1eae1444
rexescay <rx.professional@protonmail.com>
2018-07-30 00:06:19 -0400

    Changed move target to copy target for safty

--- media_sort.py
+++ media_sort.py
@@ -9,29 +9,40 @@ import os, random, re, shutil, sys
 if len(sys.argv) == 2:
 	if os.path.isdir(sys.argv[1]):
 		root_dir = sys.argv[1]
+		if not root_dir.endswith('/'):
+			root_dir += '/'
+		os.chdir(root_dir)
 else:
 	print('# python3 image_sort.py <path/to/root/directory>')
 	sys.exit()
 
-# Walk filesystem from chosen root directory
-# Check each file suffix using regex
-# each new suffix is saved in a list
-# new directory is created for each item in list
-# option to randomize filenames, not changing suffix
-# each file is moved into appropriate directory
-
 suffix_re = re.compile(r'\.[a-z]{3,4}$')
 suffix_list = []
 
+# Walk filesystem from chosen root directory
 for dir_names, sub_dirs, filenames in os.walk(root_dir):
 	for files in filenames:
+		# Check each file suffix using regex
 		if suffix_re.search(files) != None:
+			# each new suffix is saved in a list
 			if suffix_re.search(files).group() not in suffix_list:
 				suffix_list.append(suffix_re.search(files).group())
 
+# new directory is created for each item in list
 for s in suffix_list:
-	if os.path.exists(root_dir+s[1:]):
-		print(s[1:]+' directory exists')
+	s = s[1:]
+	if os.path.exists(root_dir+s):
+		print(s+' directory exists')
 	else:
-		print('Creating directory %s...'%s[1:])
-		os.makedirs(root_dir+s[1:])
+		print('Creating directory %s...'%s)
+		os.makedirs(root_dir+s)
+
+# each file is copied into appropriate directory
+print(suffix_list)
+for s in suffix_list:
+	for dir_names, sub_dirs, filenames in os.walk(root_dir):
+		for files in filenames:
+			if files.endswith(s):
+				print('Copying '+files+' to %s directory...' %s)
+				new_dir = root_dir+s[1:]
+				shutil.copy(files, new_dir)
