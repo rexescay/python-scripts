dfa4b8f9062515a92b26dcfa79102d4819e29454
rexescay <rx.professional@protonmail.com>
2018-08-08 19:38:46 -0400

    Fixed comment

--- organise.sh
+++ organise.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/sh
 
 for files in * ; do
-	echo "$files"
+	# Makes directories based on the suffixes of files in root directory
+	extentions=$(echo "$files" | cut -d '.' -f 2)
+	if [ ! -d $extentions ] ; then
+		mkdir $extentions
+	fi
+	mv $files $extentions
 done
